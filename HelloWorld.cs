fc313ff5bb76830ee9f7f779fe2188e1a567435f
Manuel Leduc <manuel.leduc@inria.fr>
2015-11-23 18:01:05 +0200

    First version

---
+++ HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using Org.Kevoree.Annotation;
+using System;
+using System.ComponentModel.Composition;
+
+namespace Org.Kevoree.Library.Comp.HelloWorld
+{
+    [ComponentType]
+    [Serializable]
+    [Export(typeof(DeployUnit))]
+    public class HelloWorld : MarshalByRefObject, DeployUnit
+    {
+
+        [Start]
+        public void Start()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+
+        [Stop]
+        public void Stop()
+        {
+            Console.WriteLine("Bye, world!");
+        }
+    }
+}
